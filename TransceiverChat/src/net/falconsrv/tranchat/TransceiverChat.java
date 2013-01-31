a639ee449764344173bb3e36892b94c9a917cb9c
Hayato OKUMOTO <falcon@falconsrv.net>
2013-01-31 20:29:56 +0900

    default parameter

--- TransceiverChat/src/net/falconsrv/tranchat/TransceiverChat.java
+++ TransceiverChat/src/net/falconsrv/tranchat/TransceiverChat.java
@@ -1,24 +1,36 @@
 package net.falconsrv.tranchat;
 
 import java.awt.EventQueue;
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 import javax.swing.UIManager;
 
 import net.falconsrv.tranchat.gui.ChatFrame;
 
 public class TransceiverChat {
+	public static InetAddress DEFAULT_ADDR;
+	public static final int DEFAULT_PORT = 12345;
+	public static final String DEFAULT_NAME = "Annonymous";
+	public static int port;
+	public static String user_name;
 
 	public static void main(String[] args) {
+		try {
+			DEFAULT_ADDR = Inet4Address.getByName("255.255.255.255");
+			port = DEFAULT_PORT;
+			user_name = DEFAULT_NAME;
+		} catch (UnknownHostException e1) {
+			e1.printStackTrace();
+		}
+
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
 					UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 					ChatFrame frame = new ChatFrame();
 					frame.setVisible(true);
-
-					PacketReceiver receiver = new PacketReceiver();
-					receiver.addPacketListner(frame);
-					receiver.start();
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
